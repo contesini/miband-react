f56542d44d21d3592d81a558439d829cc0b9ce41
allan contesini <allancontesini@hotmail.com>
2019-08-18 03:25:22 -0300

    Updates

---
+++ precache-manifest.bac85e8435bbcfd10c895730359df967.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64468fe72a73d018bf158f9dc951ff70",
+    "url": "/miband-react/index.html"
+  },
+  {
+    "revision": "2f800ee67cae0c5d3181",
+    "url": "/miband-react/static/css/main.ecf7eb69.chunk.css"
+  },
+  {
+    "revision": "615ed6b4d8753164b3dc",
+    "url": "/miband-react/static/js/2.e3f93b1a.chunk.js"
+  },
+  {
+    "revision": "2f800ee67cae0c5d3181",
+    "url": "/miband-react/static/js/main.d66ae09d.chunk.js"
+  },
+  {
+    "revision": "1a5f69823acacc8889ab",
+    "url": "/miband-react/static/js/runtime~main.f3afbe20.js"
+  }
+]);
